3f51001756a28c3e54605d706cc7ef5bb1fddb85
Estevão Kayes Marins de Souza <estevaokayes8@gmail.com>
2025-07-01 23:52:30 -0300

    Certo

--- src/contaTerminal.java
+++ src/contaTerminal.java
@@ -1,32 +1,36 @@
 import java.util.Scanner;
+
 public class contaTerminal {
 
-	public static void main(String[] args) {
-	Scanner sc = new Scanner(System.in);
-		System.out.println("======================================");
-		System.out.println("         Bem-vindo ao Badresco        ");
-		System.out.println("======================================");
-		
-		System.out.println("--------------------------------------");
-		System.out.println("Porfavor Digite o seu Nome: ");
-		String nome = sc.next();
-		System.out.println("Olá Senhor "+nome);
-		
-		System.out.println("Digite a sua Agencia ex '1234-5': ");
-		String agencia = sc.next();
-		
-		System.out.println("Perfeito Agora digite a sua Conta: ");
-		int conta = sc.nextInt();
-		
-		System.out.println("Qual o Valor voce gostaria de depositar: ");
-		double dinheiro = sc.nextDouble();
-		
-		System.out.println("--------------------------------------");
-		System.out.println("======================================");
-		System.out.printf("Nome %s \nAgencia %s \nConta %d \nValor Deposito : R$ %.2f",nome,agencia,conta,dinheiro);
-		System.out.println();
-		System.out.println("======================================");
-		
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("======================================");
+        System.out.println("         Bem-vindo ao Badresco        ");
+        System.out.println("======================================");
+
+        System.out.println("--------------------------------------");
+        System.out.print("Por favor, digite o seu nome: ");
+        String nome = sc.nextLine();
+
+        System.out.println("Olá, Senhor " + nome);
+
+        System.out.print("Digite a sua Agência (ex: 1234-5): ");
+        String agencia = sc.nextLine();
+
+        System.out.print("Perfeito! Agora digite a sua Conta: ");
+        int conta = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Qual o valor que você gostaria de depositar: ");
+        String valorStr = sc.nextLine().replace(",", ".");
+        double dinheiro = Double.parseDouble(valorStr);
+
+        System.out.println("--------------------------------------");
+        System.out.println("======================================");
+        System.out.printf("Nome: %s\nAgência: %s\nConta: %d\nValor Depósito: R$ %,.2f\n", nome, agencia, conta, dinheiro);
+        System.out.println("======================================");
+
+        sc.close();
+    }
 }
